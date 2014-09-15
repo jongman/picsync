8bc19a506ab3ed90f1ef46e2f8f21cfb8eb17712
JongMan Koo <jongman@gmail.com>
2014-09-15 14:16:34 +0900

    use jhead instead of jpegtran

--- lib/image_utils.py
+++ lib/image_utils.py
@@ -1,13 +1,21 @@
 from jpegtran import JPEGImage
-from shutil import copyfile
+from fs_utils import encode_path, copy
+from subprocess import check_output, STDOUT
 
 def autorotate(path):
     if path.split('.')[-1].lower() not in ('jpg', 'jpeg'):
         return False
 
-    img = JPEGImage(path)
-    if img.exif_orientation == 1: return False
+    output = check_output(['jhead', encode_path(path)], stderr=STDOUT).splitlines()
+    rotated = False
+    for line in output:
+        if line.startswith('Orientation'):
+            rotated = True
+            break
 
-    copyfile(path, path + '.original')
-    img.exif_autotransform().save(path)
+    if not rotated: return False
+
+    copy(path, path + '.original')
+    print check_output(['jhead', '-autorot', encode_path(path)])
+
     return True
